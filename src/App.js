3aebf84f2289a2a39167d1f9c04708282533092e
morpole <morpole@gmail.com>
2023-02-02 16:43:01 +0000

    added useState Counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter.js";
+import Setup from "./tutorial/1-useState/setup/3-useState-array.js";
 
 function App() {
   return (
